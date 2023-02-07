b258467fb5895430a166932d84ee97b1de97724d
Paul Duncan <paulddncn@gmail.com>
2023-02-06 18:58:57 -0700

    pseudo notes

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("goodbye")
+//As a user I want to be able to pull up the page and see a search bar
+    // add event listener to the search button
+    // needs to perform a fetch for getLocation
+    // if search is invalid show 404 error dynamically populated on the page
+
+// When I input a search I see a 5 day forecast for the area along with a list of hiking trails nearby
+    //fetch lat and lon through our geoCode API
+    // use that data to create our URL for the five day forecast 
+    // create divs and internal elements to host this information
+    //dynamically populate the divs and elements
+
+// When I look at the forecast I see temp, rain, wind, humidity, sunrise and sunset 
+    //create "card" div with list or p elements to note all the specific weather information
+    // use an API to show the weather icons
+
+// When I look at the trails list, I can see photos of the trails and maybe links to trail websites or reviews
+    // fetch API data to pull list of trails
+    // get photos to populate with the trails on our page 
